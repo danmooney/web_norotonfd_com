618a251e07c558923cf38da21ea5d34fc80faf7b
Dan Mooney <dan.mooney@mminc.co>
2013-02-02 23:38:45 -0500

    Templating

--- templates/noroton/index.php
+++ templates/noroton/index.php
@@ -1,13 +1,19 @@
 <?php
-defined('_JEXEC') or die('Restricted Access.'); ?>
+defined('_JEXEC') or die('Restricted Access.');
+
+require_once('setup.php');
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
+    <script src="templates/<?= $this->template ?>/js/jquery-1.9.0-src.js"></script>
+    <jdoc:include type="head" />
     <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700' rel='stylesheet' type='text/css' />
     <link href='http://fonts.googleapis.com/css?family=Share' rel='stylesheet' type='text/css' />
     <link rel="stylesheet" type="text/css" href="templates/<?= $this->template ?>/css/template.css" />
 </head>
-<body>
+<body class="<?= $body_class ?>">
     <div id="header">
         <div id="header-bg"></div>
         <div class="content-container">
@@ -17,16 +23,18 @@ defined('_JEXEC') or die('Restricted Access.'); ?>
             <jdoc:include type="modules" name="top-menu" />
             <jdoc:include type="modules" name="main-menu" />
             <div class="clr"></div>
+            <div id="hero-image-container-bg"></div>
             <div id="hero-image-container">
                 <div id="hero-image"></div>
+
             </div>
         </div>
     </div>
-    <div id="content">
+    <div id="content" class="<?= $content_class ?>">
         <div id="content-bg">
             <div id="content-bg-top"></div>
             <div class="content-container">
-                The Noroton Fire Department
+                <jdoc:include type="component" />
             </div>
         </div>
     </div>
@@ -62,5 +70,9 @@ defined('_JEXEC') or die('Restricted Access.'); ?>
             <jdoc:include type="modules" name="external-links-menu" />
             <p>Designed by <a class="bold" target="_blank" href="http://scsdesigninc.com">ScSDesignInc.com</a> and developed by <a target="_blank" class="bold" href="http://www.danronmoon.com/">DanRonMoon.com</a></p>
         </div>
+    </div>
+    <div id="image-cache">
+        <img src="templates/<?= $this->template ?>/img/header_fb_hover.png" />
+        <img src="templates/<?= $this->template ?>/img/header_tw_hover.png" />
     </div>
 </body>
---
+++ templates/noroton/setup.php
@@ -0,0 +1,79 @@
+<?php
+
+function debug($string, $dieBool = true) {
+    if ($_SERVER['REMOTE_ADDR'] !== '71.9.158.10' &&
+        $_SERVER['REMOTE_ADDR'] !== '108.200.220.35'
+    ) {
+        return;
+    }
+
+    print_r($string);
+    if (true === $dieBool) {
+        die;
+    }
+}
+
+$input = JFactory::getApplication()->input;
+
+$id     = (int) $input->get('id', 0, 'int');
+$itemId = (int) $input->get('Itemid', 0, 'int');
+
+$option = $input->get('option', null, 'string');
+
+$menu = JMenu::getInstance('site');
+
+$isFrontpage = ($menu->getActive() === $menu->getDefault());
+
+$isArticle   = ($input->get('view') === 'article');
+
+$active_menu = null;
+
+if ($itemId > 0 && !$isFrontpage) {
+    $article = JTable::getInstance('content');
+    $article->load($id);
+    $active_menu = $menu->getActive();
+    $parent_id = (int) $active_menu->parent_id;
+
+    if ('com_content' === $option) {
+        $body_class = $article->get('alias');
+    } else {
+        $body_class = preg_replace('/(com_)(moo)?/', '', $option);
+    }
+} else {
+    $body_class = '';
+    $menu_title = '';
+}
+
+if ($isFrontpage) {
+    $body_class = 'home';
+    $content_class = 'frontpage';
+
+    $document  = JFactory::getDocument();
+    $doc_title = $document->getTitle();
+    $site_title = JFactory::getApplication()->getCfg('sitename');
+    $document->setTitle($doc_title . ' - ' . $site_title);
+
+} else {
+    $content_class = 'article';
+}
+
+// check session success message
+$session = JFactory::getSession();
+$success_message = $session->get('success_message');
+$error_message   = $session->get('error_message');
+
+if ($success_message || $error_message) {
+    $message_type = $success_message
+        ? 'success'
+        : 'error';
+
+    $message_var_str = $message_type . '_message';
+    $message = $$message_var_str;
+
+    $has_messages_bool = true;
+} else {
+    $has_messages_bool = false;
+}
+
+$session->clear('success_message');
+$session->clear('error_message');
