72eb2a9cd2a009d037ff345dd6928e8e6d5d5bb1
danmooney <dan.mooney@moodigital.com>
2013-02-27 21:27:59 -0500

    Fundraiser module work

--- modules/mod_moo_fundraiser/mod_moo_fundraiser.php
+++ modules/mod_moo_fundraiser/mod_moo_fundraiser.php
@@ -8,8 +8,8 @@ defined('_JEXEC') or die('Restricted Access');
 require_once(JPATH_SITE . DS . 'modules' . DS . '_view_helper.php');
 require_once('model.php');
 
-$model = new MooExternalLinksModel();
-$links = $model->getData();
+$model = new MooFundraiserModel();
+$events = $model->getData();
 $helper = new MooViewHelper();
 
-require JModuleHelper::getLayoutPath('mod_moo_externallinks');
+require JModuleHelper::getLayoutPath('mod_moo_fundraiser');
