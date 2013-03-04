3430a085f14847e7bb449b4ade465c7e1e8a6663
danmooney <dan.mooney@moodigital.com>
2013-03-04 13:22:45 -0500

    Successful login, callback needed

--- templates/noroton/login.php
+++ templates/noroton/login.php
@@ -10,6 +10,7 @@ defined('_JEXEC') or die;
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
+    <script src="templates/<?= $this->template ?>/js/jquery-1.9.0-src.js"></script>
 	<jdoc:include type="head" />
 	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
 	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
@@ -24,5 +25,30 @@ defined('_JEXEC') or die;
         <h3 style="text-align:center">Members Log In</h3>
 	    <jdoc:include type="component" />
     </div>
+<script type="text/javascript">
+    jQuery(document).ready(function ($) {
+        $('form').on('submit', function (e) {
+            e.preventDefault();
+
+            $.ajax({
+                type: 'POST',
+                url: '/',
+                data: {
+                    ajax: 1,
+                    username: $('#modlgn-username').val(),
+                    password: $('#modlgn-passwd').val()
+                },
+                success: function (data) {
+                    console.log(data);
+                },
+                error: function (data) {
+                    console.log(data);
+//                    alert('An error has occurred.  Please try again later.');
+                }
+            });
+
+        });
+    });
+</script>
 </body>
 </html>
