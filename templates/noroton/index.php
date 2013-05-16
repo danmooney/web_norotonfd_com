2c23ece20a7e08b43c9030f9a00bc791fe35cf24
danmooney <dan.mooney@moodigital.com>
2013-05-15 20:35:08 -0400

    Add GA and closing HTML tag (!)

--- templates/noroton/index.php
+++ templates/noroton/index.php
@@ -90,6 +90,7 @@ require_once('setup.php');
             </div>
         </div>
     </div>
+    
     <div id="footer">
         <div id="footer-bg-top"></div>
         <div class="content-container center">
@@ -118,4 +119,16 @@ require_once('setup.php');
         <img src="templates/<?= $this->template ?>/img/arrow_right_orange.png" />
         <img src="templates/<?= $this->template ?>/img/form_label_error_triangle.png" />
     </div>
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-39382788-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
 </body>
+</html>
